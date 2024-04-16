df6d9429e7f04ed517ec6dbd704cc3b5e4619fdb
isabellkaitlyn <isabell.allard@gmail.com>
2024-04-16 16:34:25 -0700

    Initial commit

---
+++ Assets/Scripts/Welcome.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Welcome : MonoBehaviour
+{
+    public void StartGame ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
